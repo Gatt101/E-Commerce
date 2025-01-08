8d71775b30bea636b9576ab197750f60563e72f2
Gatt101 <gauravpatilk11@gmail.com>
2025-01-08 14:35:21 +0530

    creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LayoutComponent,children:[
+        {path:'',redirectTo: 'home',pathMatch: 'full'},
+        {path: 'home',component:HomeComponent},
+        {path:'Login',component:LoginComponent},
+        {path:"register",component:RegisterComponent},
+    ]},
+];
