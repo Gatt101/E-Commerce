7c29b7b846ae5942e81da93fde725727010138fb
Gatt101 <gauravpatilk11@gmail.com>
2025-01-08 21:50:55 +0530

    light/dark mode updated

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -1,17 +1,29 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,NgClass],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-toggleTheme() {
+  constructor() { }
+
+  ngOnInit() 
+  {
+    // Add code to check if user i
+  }
+
+  red:boolean = false;
+  blue:boolean = false;
+
+toggleTheme() 
+{
   // Add code to toggle theme
-  
+  this.red =!this.red;
 }
 
 }
