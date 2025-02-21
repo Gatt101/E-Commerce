0e0effdd61c9de2b2374e6295306a078fff9742e
Gatt101 <gauravpatilk11@gmail.com>
2025-02-21 23:58:30 +0530

    remove newletter

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,25 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CategoryService } from '../../service/category.service';
-import { NewsletterService } from '../../service/newsletter.service';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  email: string = '';
-  isSubscribing: boolean = false;
-
   constructor(
     private router: Router,
-    private categoryService: CategoryService,
-    private newsletterService: NewsletterService
+    private categoryService: CategoryService
   ) {}
 
   products() {
@@ -35,28 +29,5 @@ export class HomeComponent {
   productsforwomen() {
     this.categoryService.setCategory('women\'s clothing');
     this.router.navigate(['/product']);
-  }
-
-  subscribeNewsletter(event: Event) {
-    event.preventDefault();
-    if (!this.email) {
-      alert('Please enter an email address');
-      return;
-    }
-    
-    this.isSubscribing = true;
-    this.newsletterService.subscribe(this.email).subscribe({
-      next: () => {
-        alert('Thank you for subscribing to our newsletter!');
-        this.email = '';
-      },
-      error: (error) => {
-        console.error('Newsletter subscription failed:', error);
-        alert('Failed to subscribe. Please try again later.');
-      },
-      complete: () => {
-        this.isSubscribing = false;
-      }
-    });
   }
 }
